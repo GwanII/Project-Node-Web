2a9433edc9139b9d4bdb23e3e0e55caedc776ec9
GwanII <dhksl999@gmail.com>
2026-08-02 22:17:41 +0900

    set base
    기초 세팅 완료

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '50px', textAlign: 'center' }}>
+      <h1>🎉 우리의 첫 팀플 웹 서비스!</h1>
+      <p>여기가 바로 리액트로 화면을 그리는 곳입니다.</p>
+    </main>
+  );
+}
